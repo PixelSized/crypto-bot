88b620311f9371d840ed076f49abf85dbf64d20f
Jenk Scalia <41992596+PixelSized@users.noreply.github.com>
2021-05-07 19:19:00 +1000

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 import preprocessing
 
 data = {
-    "coin": "BTC-AUD"  #The Coin we are reading
+    "coin": "BTC-AUD",  #The Coin we are reading
+    "period": "5d", # valid periods: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max  # (optional, default is '1mo')
+    "interval": "5m" # valid intervals: 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo
 }
 
 
