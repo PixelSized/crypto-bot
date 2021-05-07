e4c249829ba615566184a250eb95acfae218bf16
Jenk Scalia <41992596+PixelSized@users.noreply.github.com>
2021-05-07 21:20:45 +1000

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ data = {
     "coin": "BTC-AUD",  #The Coin we are reading
     "period": "5d", # valid periods: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max  # (optional, default is '1mo')
     "interval": "5m", # valid intervals: 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo
+    "refresh": "300", # this should be the amount of seconds oh whatever the interval is (secs = mins x 60)
     "theme": "plotly_dark" #Available themes: 'ggplot2', 'seaborn', 'simple_white', 'plotly', 'plotly_white', 'plotly_dark', 'presentation', 'xgridoff', 'ygridoff', 'gridon', 'none'
 }
 
